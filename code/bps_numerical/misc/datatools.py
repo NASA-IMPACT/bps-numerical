0058f5dd4c4fa76cf5fe29ce6cf65d5c4ad036c3
NISH1001 <nishanpantha@gmail.com>
2022-06-30 13:10:41 -0500

    Add basics data loader and preprocessing modules
    `bps_numerical.misc.datatools` has misc data utility
    
    `bps_numerical.preprocessing` has preprocessing items.

---
+++ code/bps_numerical/misc/datatools.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import os
+import random
+import sys
+
+from typing import Optional
+
+import pandas as pd
+
+
+def load_csv(
+    fname: str, p: float = 0.1, random_sampling: bool = False, n_samples: Optional[int] = None
+) -> pd.DataFrame:
+    """
+    Loads csv partially or fully.
+    Also allows random sampling
+
+    Args:
+        fname: `str`
+            Input csv path
+
+        p: `float`
+            How many rows to load if random_sampling is enabled
+
+    """
+    if not random_sampling:
+        if isinstance(n_samples, int) and n_samples > 0:
+            return pd.read_csv(fname, nrows=n_samples)
+        else:
+            return pd.read_csv(fname)
+    return pd.read_csv(fname, header=0, skiprows=lambda i: i > 0 and random.random() > p)
+
+
+def main():
+    src = "/Users/nishparadox/dev/uah/nasa-impact/gene-experiments/data/merged.csv"
+    src = "/Users/nishparadox/dev/uah/nasa-impact/gene-experiments/data/meta-2.csv"
+    df = load_csv(src, p=0.1, n_samples=None, random_sampling=True)
+    print(df.shape)
+
+
+if __name__ == "__main__":
+    main()
---
+++ code/bps_numerical/preprocessing.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+import pandas as pd
+
+from typing import Union
+
+from loguru import logger
+
+from .misc.datatools import load_csv
+
+
+def standardize_gene_data(fname: Union[str, pd.DataFrame]) -> pd.DataFrame:
+    """
+    This applies:
+        - transposes the original csv matrix so that each row becomes a list of
+        gene features
+        - changes 'gene' column name to 'Sample' (phenotype/metadata csv
+        has 'Sample' as a column name)
+
+    Args:
+        fname: `str` or `pd.DataFrame`
+            Input original gene csv or dataframe
+
+    Returns:
+        `pd.DataFrame`
+    """
+    logger.info("Standardizing gene data into proper format.")
+    df = fname
+    if isinstance(fname, str):
+        df = pd.read_csv(fname, chunksize=10000, iterator=False)
+        df = pd.concat(df, ignore_index=False)
+
+    df = df.T.reset_index()
+    df.rename(columns=df.iloc[0], inplace=True)
+    df.drop(df.index[0], inplace=True)
+
+    # for speed, linear search
+    if "gene" in df.columns[:100]:
+        df.rename(columns={"gene": "Sample"}, inplace=True)
+    return df
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
